1380455838292edaab98669a2d80b67762026991
shubham21699 <shubham21699@gmail.com>
2021-12-07 23:08:25 +0530

    Delete saved links button and functionality added

--- script.js
+++ script.js
@@ -3,33 +3,54 @@ const inputElement = document.getElementById("input-element")
 const inputBtn = document.getElementById("input-btn")
 const ulElement = document.getElementById("ul-element")
 
+
+// Check if there is any data in local storage and if it is there then parse it as JS array
+const tabsFromLocalStorage = JSON.parse(localStorage.getItem("myTabs"))
+
+// Check if tabsFromLocalStorage is truthy and if so then sey myTabs to its value and call renderTabs()
+if(tabsFromLocalStorage) {
+    myTabs = tabsFromLocalStorage
+    render(myTabs)
+}
+
+const deleteBtn = document.getElementById("delete-btn")
+// When double clicked on delete button, clear local storage, myTabs and the DOM
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myTabs = []
+    render(myTabs)
+})
+
+
 inputBtn.addEventListener("click", function() {
     myTabs.push(inputElement.value)
     inputElement.value = ""
-    renderTabs() 
+
+    // localStorage stores only string, so we use first JSON.stringify() to convert our array to 
+    // string and then use JSON.parse() to store that string as proper array again 
+    localStorage.setItem("myTabs", JSON.stringify(myTabs)) 
+
+    render(myTabs) 
 })
 
-function saveTab() {
-    
-}
-
-function renderTabs() {
+// Render the links stored in array passed on calling render() function
+function render(links) {
 
     let listItems = ""
 
     // Log out items in myTabs array using for loop
-    for(let i=0 ; i<myTabs.length ; i++) {
+    for(let i=0 ; i<links.length ; i++) {
 
         // listItems += "<li><a target='_blank' href='" + myTabs[i] + "'>" + myTabs[i] + "</a></li>"
         listItems += `<li>
-                        <a target='_blank' href='${myTabs[i]}"'> 
-                            ${myTabs[i]} 
+                        <a target='_blank' href='${links[i]}"'> 
+                            ${links[i]} 
                         </a>
                     </li>`
-        // ulElement.innerHTML += "<li>" + myTabs[i] + "</li>"
+        // ulElement.innerHTML += "<li>" + links[i] + "</li>"
         // OR: another method of doing same as above line
         // const li = document.createElement("li")
-        // li.textContent = myTabs[i];
+        // li.textContent = links[i];
         // ulElement.append(li)
 
     }

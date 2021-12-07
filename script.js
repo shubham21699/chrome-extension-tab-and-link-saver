285a985b76f82face66d22d2a72ae7cda5c11f3e
shubham21699 <shubham21699@gmail.com>
2021-12-07 19:22:15 +0530

    save link button functionality added

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
+let myTabs = []
+const inputElement = document.getElementById("input-element")
+const inputBtn = document.getElementById("input-btn")
+const ulElement = document.getElementById("ul-element")
+
+inputBtn.addEventListener("click", function() {
+    myTabs.push(inputElement.value)
+    inputElement.value = ""
+    renderTabs() 
+})
+
 function saveTab() {
     
 }
 
-let inputBtn = document.getElementById("input-btn")
+function renderTabs() {
 
-inputBtn.addEventListener("click", function() {
-    
-})
+    let listItems = ""
+
+    // Log out items in myTabs array using for loop
+    for(let i=0 ; i<myTabs.length ; i++) {
+
+        // listItems += "<li><a target='_blank' href='" + myTabs[i] + "'>" + myTabs[i] + "</a></li>"
+        listItems += `<li>
+                        <a target='_blank' href='${myTabs[i]}"'> 
+                            ${myTabs[i]} 
+                        </a>
+                    </li>`
+        // ulElement.innerHTML += "<li>" + myTabs[i] + "</li>"
+        // OR: another method of doing same as above line
+        // const li = document.createElement("li")
+        // li.textContent = myTabs[i];
+        // ulElement.append(li)
+
+    }
+
+    // Render the listItems inside the unordered list using ulElement.innerHTML
+    ulElement.innerHTML = listItems
+
+}

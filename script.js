0504ab699cd6a702dd09ba95c8260721392820a5
shubham21699 <shubham21699@gmail.com>
2021-12-08 13:09:37 +0530

    Save tab button and functionality added

--- script.js
+++ script.js
@@ -1,36 +1,53 @@
-let myTabs = []
+let myLinks = []
 const inputElement = document.getElementById("input-element")
 const inputBtn = document.getElementById("input-btn")
 const ulElement = document.getElementById("ul-element")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
 
 // Check if there is any data in local storage and if it is there then parse it as JS array
-const tabsFromLocalStorage = JSON.parse(localStorage.getItem("myTabs"))
+const tabsFromLocalStorage = JSON.parse(localStorage.getItem("myLinks"))
 
 // Check if tabsFromLocalStorage is truthy and if so then sey myTabs to its value and call renderTabs()
 if(tabsFromLocalStorage) {
-    myTabs = tabsFromLocalStorage
-    render(myTabs)
+    myLinks = tabsFromLocalStorage
+    render(myLinks)
 }
 
-const deleteBtn = document.getElementById("delete-btn")
 // When double clicked on delete button, clear local storage, myTabs and the DOM
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
-    myTabs = []
-    render(myTabs)
+    myLinks = []
+    render(myLinks)
+})
+
+
+tabBtn.addEventListener("click", function() {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // since only one tab should be active and in the current window at once
+        // the return variable should only have one entry
+        // let activeTab = tabs[0]
+        // let activeTabId = activeTab.id
+
+        myLinks.push(tabs[0].url)
+        localStorage.setItem("myLinks", JSON.stringify(myLinks)) 
+        render(myLinks)
+    })
+
 })
 
 
 inputBtn.addEventListener("click", function() {
-    myTabs.push(inputElement.value)
+    myLinks.push(inputElement.value)
     inputElement.value = ""
 
     // localStorage stores only string, so we use first JSON.stringify() to convert our array to 
     // string and then use JSON.parse() to store that string as proper array again 
-    localStorage.setItem("myTabs", JSON.stringify(myTabs)) 
+    localStorage.setItem("myLinks", JSON.stringify(myLinks)) 
 
-    render(myTabs) 
+    render(myLinks) 
 })
 
 // Render the links stored in array passed on calling render() function
